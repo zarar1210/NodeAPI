ff260358921e321a5b308df458ee9e2472f51de9
Zarar Ali <zarar.ali@sundaymobility.com>
2024-12-01 22:08:32 +0530

    bug fix

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get("/user", (req, res) => {
 
 const port = process.env.port || 3000;
 
-app0.listen(port, function () {
+app.listen(port, function () {
   console.log(`I am running on ${port} `);
 });
