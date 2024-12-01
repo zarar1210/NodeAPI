7c6f0b506b40fe95289f6a1810943ae6f28f24f8
Zarar Ali <zarar.ali@sundaymobility.com>
2024-12-01 22:34:20 +0530

    bug fix

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/", (req, res) => {
   res.json({
     message: "This is api using user end point",
   });
 });
 
-const port = process.env.port || 5002;
+const port = 5003;
 
 app.listen(port, function () {
   console.log(`I am running on ${port} `);
