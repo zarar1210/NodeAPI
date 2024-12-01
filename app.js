5bdc3bb65b12857a40e01a2bb896380ffb1a7c9b
Zarar Ali <zarar.ali@sundaymobility.com>
2024-12-01 12:35:39 +0530

    Create new user api

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.json({
+    message: "This is api using user end point",
+  });
+});
+
 const port = process.env.port || 3000;
 app.listen(port, function () {
-  console.log("I am running do not worry");
+    console.log("Server is running");
 });
