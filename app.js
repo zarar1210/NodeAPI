94a1e6420d81bdec8044f9b976ce1bd86eef4989
Zarar Ali <zarar.ali@sundaymobility.com>
2024-12-03 18:11:00 +0530

    add new end point

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("ap/get", (req, res) => {
+  res.json({
+    message: "This functionality implementd via CICD pipeline from github",
+  });
+});
+
+app.get("ap/get_user_details", (req, res) => {
   res.json({
     message: "This functionality implementd via CICD pipeline from github",
   });
@@ -9,7 +15,7 @@ app.get("/", (req, res) => {
 
 const port = 5003;
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log('Server is running');
 });
 
